c01234979bfcac39c347323eba7a4798992ecb07
Dan Kuzin <shadows_song@mail.ru>
2023-05-04 22:14:15 +0300

    added zipkin distributed tracing

--- FrontService/src/main/java/com/kuzin/frontservice/FrontApp.java
+++ FrontService/src/main/java/com/kuzin/frontservice/FrontApp.java
@@ -1,6 +1,7 @@
 package com.kuzin.frontservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -8,6 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableAutoConfiguration
 public class FrontApp {
     public static void main(String[] args) {
         SpringApplication.run(FrontApp.class);
